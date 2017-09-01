8992a75649cf1bcba8ef159e45f8a068be43e0ed
Ross Melville <rossmelville@hotmail.com>
2017-09-01 13:57:06 +0100

    Added test to add task to heros tasks.

--- specs/hero_integration_spec.js
+++ specs/hero_integration_spec.js
@@ -10,6 +10,8 @@ describe("Hero, Task and Food integration", function(){
     apple = new Food("Apple", 10);
     bread = new Food("Bread", 5);
     task1 = new Task("Save Zelda", 10, 1, "Big pat on the back");
+    task2 = new Task("Defeat Ganon", 9, 2, "Pint in the pub");
+    task3 = new Task("Collect Master Sword", 3, 3, "Better kill rate");
   });
 
   it("Hero can eat food to replenish health", function(){
@@ -22,7 +24,16 @@ describe("Hero, Task and Food integration", function(){
     link.reduceHealth(50);
     link.eat(apple);
     assert.strictEqual(link.health, 65);
-  })
+  });
+
+  it("Can add task to heros tasks", function(){
+    link.addTask(task1);
+    assert.strictEqual(link.tasks[0], task1);
+  });
+
+  // it("Sort hero tasks by difficulty", function(){
+
+  // })
 
 
 })
