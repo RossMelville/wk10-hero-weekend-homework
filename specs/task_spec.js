323163bed7ef6be26bbe6dc995111ba040873c71
Ross Melville <rossmelville@hotmail.com>
2017-09-01 11:46:06 +0100

    Added to the task_spec.js so it is set up for tests, constructor required for Task object.

---
+++ specs/task_spec.js
@@ -0,0 +1,11 @@
+var Task = require("../hero.js");
+var assert = require("assert");
+
+describe("Task Object", function(){
+
+  beforeEach(function(){
+    task = new Task("Catch the joker", 10, 1, "Big pat on the back");
+  });
+
+  
+})
