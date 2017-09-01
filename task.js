c4901c02da2cd370a0b0a80e6f1b1909e38d2808
Ross Melville <rossmelville@hotmail.com>
2017-09-01 11:49:27 +0100

    Added constructor function to Task class.

---
+++ task.js
@@ -0,0 +1,9 @@
+var Task = function(description, difficulty, priority, reward){
+  this.description = description;
+  this.difficulty = difficulty;
+  this.priority = priority;
+  this.reward = reward;
+};
+
+
+module.exports = Task;
