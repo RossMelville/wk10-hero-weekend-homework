0a87191e82ad27a2439498aed5f6399771d33cb6
Ross Melville <rossmelville@hotmail.com>
2017-09-01 12:07:09 +0100

    Added test to task_spec to check task has description, TDD test passed.

--- specs/task_spec.js
+++ specs/task_spec.js
@@ -1,11 +1,14 @@
-var Task = require("../hero.js");
+var Task = require("../task.js");
 var assert = require("assert");
 
 describe("Task Object", function(){
 
   beforeEach(function(){
-    task = new Task("Catch the joker", 10, 1, "Big pat on the back");
+    task = new Task("Catch the Joker", 10, 1, "Big pat on the back");
   });
 
-  
+  it("Should have description", function(){
+    assert.strictEqual(task.description, "Catch the Joker")
+  });
+
 })
