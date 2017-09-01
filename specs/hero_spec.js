48f856917e4a8eb3d2b625332a56da3c37018e54
Ross Melville <rossmelville@hotmail.com>
2017-09-01 10:58:57 +0100

    Initial commit. Created files and spec files for Hero, Task, Food objects.

---
+++ specs/hero_spec.js
@@ -0,0 +1,12 @@
+var Hero = require("../hero.js");
+var assert = require("assert");
+
+describe("Hero Object", function(){
+
+  beforeEach(function(){
+    hero = new Hero("Batman", 100, "Pizza");
+  })
+
+
+
+})
